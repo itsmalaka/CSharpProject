4eee7b8e91ea991534222bb8a663dd6981f43e79
itsmalaka <khlokmalaka@gmail.com>
2014-12-26 17:02:18 -0600

    read json and xml, generate xml classes

---
+++ RESTServicesJSONParserExample/RESTServicesJSONParserExample/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RESTServicesJSONParserExample
+{
+   public class channel
+    {
+        public string title { get; set; }
+        public string link { get; set; }
+        public string description { get; set; }
+    }
+}
--- RESTServicesJSONParserExample/RESTServicesJSONParserExample/Program.cs
+++ RESTServicesJSONParserExample/RESTServicesJSONParserExample/Program.cs
@@ -4,6 +4,8 @@ using System.Web;
 using System.Web.Script.Serialization;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
 
 namespace RESTServicesJSONParserExample
 {
@@ -34,12 +36,28 @@ namespace RESTServicesJSONParserExample
             Developer
         }
 
+        public class Weather
+        {
+            public Coord Coord { get; set; }
+        }
+
+
+        public class Coord
+        {
+            public string lon { get; set; }
+            public string lat { get; set; }
+        }
+
+
         static void Main(string[] args)
         {
-            string url = @"C:\Users\kay\Documents\GitHub\CSharpProject\data.json";
+            //string url = @"C:\Users\aa\Documents\GitHub\CSharpProject\data.json";
+            string url = @"http://api.openweathermap.org/data/2.5/weather?q=minneapolis";
             Console.WriteLine("Hello World!!!");
 
-            Company c = Serializer(url);
+            Company c = SerializerJson(url);
+
+            SerializerXML("https://news.ycombinator.com/rss");
 
             foreach (var v in c.Employees)
             {
@@ -50,15 +68,53 @@ namespace RESTServicesJSONParserExample
             Console.Read();
         }
 
-
-        static public Company Serializer(string url)
+        public static string GetContent(string url)
         {
-            string json = File.ReadAllText(url);
+            String responseString;
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                    throw new Exception(String.Format(
+                    "Server error (HTTP {0}: {1}).",
+                    response.StatusCode,
+                    response.StatusDescription));
+                // process the response here
+                using (Stream stream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                    responseString = reader.ReadToEnd();
 
-            Company company = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Company>(json);
+                }
+                return responseString;
+            }
+        }
 
-            //Company company = new Company();
+        static public Company SerializerJson(string url)
+        {
+            string str = GetContent(url);
+                var obj = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Weather>(str);
+                
+            Company company = new Company();
             return company;
         }
+
+        static void SerializerXML(string url)
+        {
+            string str = GetContent(url);
+            //str = str.Replace("<rss version=\"2.0\">", "").Replace("<rss>","");
+            StringReader reader = new StringReader(str);
+            XmlSerializer serializer = new XmlSerializer(typeof(rss));
+            rss r =  (rss)serializer.Deserialize(reader);
+
+            foreach (var item in r.channel[0].item)
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine(item.title);
+                Console.WriteLine("\t" + item.link);
+            }
+            Console.Read();
+        }
     }
+
 }
