b9cfd6cd613033d19a4c629a0335426c0fa7db7e
itsmalaka <khlokmalaka@gmail.com>
2014-12-26 14:35:37 -0600

    add the project files

---
+++ RESTServicesJSONParserExample/RESTServicesJSONParserExample/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Collections.Generic;
+
+namespace RESTServicesJSONParserExample
+{
+    class Program
+    {
+        public class Company
+        {
+            public string Title { get; set; }
+            public List<Employee> Employees { get; set; }
+        }
+
+        /// <summary>
+        /// Employee info
+        /// </summary>
+        public class Employee
+        {
+            public string Name { get; set; }
+            public EmployeeType EmployeeType { get; set; }
+        }
+
+        /// <summary>
+        /// Types of employees
+        /// (just to se how JSON deals with enums!)
+        /// </summary>
+        public enum EmployeeType
+        {
+            CEO,
+            Developer
+        }
+
+        static void Main(string[] args)
+        {
+            string url = @"C:\Users\kay\Documents\GitHub\CSharpProject\data.json";
+            Console.WriteLine("Hello World!!!");
+
+            Company c = Serializer(url);
+
+            foreach (var v in c.Employees)
+            {
+                Console.WriteLine(v.Name);
+            }
+            
+
+            Console.Read();
+        }
+
+
+        static public Company Serializer(string url)
+        {
+            string json = File.ReadAllText(url);
+
+            Company company = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Company>(json);
+
+            //Company company = new Company();
+            return company;
+        }
+    }
+}
---
+++ RESTServicesJSONParserExample/RESTServicesJSONParserExample/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RESTServicesJSONParserExample
+{
+    class Response
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+        public string score { get; set; }
+    }
+}
